1ee3fe5de0faa4f3f991a72d427eb6ed6115839f
AdelsonANS <57421602+AdelsonANS@users.noreply.github.com>
2020-04-06 18:26:02 -0300

    relatos de integrantes

--- src/script.js
+++ src/script.js
@@ -5,13 +5,23 @@ $('#carouselExampleControls').on('slide.bs.carousel', function () {
   myFunction('direita');
 })
 
+var relato = [
+  {
+    texto: "Ser um dos fundadores da Seed a Bit me traz um sentimento de eterna conexão com a empresa. Participei de todo processo de fundação e federação junto à galera massa da minha época. O primeiro contrato eu tava lá pra assinar. Nos eventos do MEJ eu tava lá levando a nossa bandeira e gritando, feito louco, que a Seed a Bit seria gigante um dia. Foram 2 anos intensos. Hoje, depois de 1 ano da minha saída percebi de longe que nossa Seed a Bit voou demais e isso se deve muito ao trabalho de todo mundo que passa por ela. É garra, determinação, foco. É vontade de ser Seed a Bit e de levar isso no coração pra sempre. Se essa oportunidade está aberta para você, agarre e aproveite da melhor forma possível! #voaseed 💙",
+    ass: "- Felipe Cavalcanti (Estudante do Curso de Ciência da Computação)"
+  },
+  {
+    texto: "Ser Analista Comercial na Seed a Bit foi uma experiência onde aprendi e desenvolvi habilidades que eu não conhecia tanto profissional quanto pessoal. Entender na prática como funciona uma empresa e a troca de conhecimento com outras áreas foi muito enriquecedor para mim. Além disso, poder participar de um meio onde vários universitários engajados através da tecnologia tem o propósito de mudar a nossa realidade foi  uma experiência incrível.",
+    ass :"- Andressa Santana (Estudante do Curso de Sistemas de Informação)"
+  },
+  {
+    texto: "Acredito que os desafios nos movem, não importam seus tamanhos. Criamos a Seed por vislumbramos um desafio, o de transformar a realidade na vida de cada aluno, seja de computação ou não, dos nossos cursos e departamentos, de cada pessoa que tem um breve contato com ela.Traçar planos, iniciar uma caminhada, criar uma empresa e ajudar os nossos clientes. Aos poucos escrevendo nossa história e auxiliando tantas outras empresas a também escreverem as suas. A Seed foi feita para cada um de vocês superarem seus próprios limites. Acreditem, conquistem, não se limitem, façam parte dessa história, sejam Seed a Bit.",
+    ass :"- Mateus Silva (Estudante do Curso de Ciência da Computação)"
+  }
+  
 
+]
 
-const textos = [
-"1-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-"2-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-"3-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text."
-];
 
 var arrow = 0;
 
@@ -20,23 +30,24 @@ function myFunction(refer){
   
 
   if(refer=='direita'){
-    if(arrow==textos.length-1){
+    if(arrow==relato.length-1){
       arrow = '0';
     }else{
       arrow++;
     }
     
-    document.getElementById('falas').innerHTML = textos[arrow];
-
+    document.getElementById('falas').innerHTML = relato[arrow].texto;
+    document.getElementById('ass').innerHTML = relato[arrow].ass;
   }
   if(refer=='esquerda'){
     if(arrow==0){
-      arrow = textos.length-1;
+      arrow = relato.length-1;
     }else{
       arrow--;
     }
     
-    document.getElementById('falas').innerHTML = textos[arrow];
+    document.getElementById('falas').innerHTML = relato[arrow].texto;
+    document.getElementById('ass').innerHTML = relato[arrow].ass;
   }
 }
 
